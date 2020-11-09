f17e3e18a6be2d7972a9a4ac1055a8b389411ad4
Luka Petrovic <petrovic.luka99@gmail.com>
2020-11-09 18:46:50 +0100

    Custom formatter

--- UDF-GUI/src/main/java/main/Main.java
+++ UDF-GUI/src/main/java/main/Main.java
@@ -11,41 +11,6 @@ public class Main {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-//		try {
-//			Class.forName("formatter.FormatterImpl");
-//			
-//			DataFormatter formatter = UDFManager.getFormatter();
-//			HashMap<String, Object> attributes = new HashMap<String, Object>();
-//			attributes.put("year", 21);
-//			attributes.put("name", "Luka");
-//			
-//			Entity child = new Entity(2, "Person", attributes, null);
-//			HashMap<String, Entity> children = new HashMap<String, Entity>();
-//			children.put("child", child);
-//			
-//			Entity entity = new Entity(1, "Person", attributes, children);
-//			
-//			List<Entity> entities = new ArrayList<Entity>();
-//			entities.add(entity);
-//			entities.add(child);
-//			
-//			String projectPath = System.getProperty("user.dir");
-//			String savePath = projectPath + "/src/main/resources";
-//			File file = new File(savePath);
-//			System.out.println(savePath);
-//			
-//			formatter.save(entities, file);
-//			
-//			List<Entity> output = formatter.read(file);
-//			
-//			for (Entity e : output) {
-//				System.out.println(e.toString());
-//			}
-//			
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 }
